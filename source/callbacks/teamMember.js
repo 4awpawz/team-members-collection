3b375dc3b4c54cb2156f9bbc1c6891d6c335dcca
Jeff Schwartz <jefftschwartz@gmail.com>
2019-10-15 22:22:23 -0400

    removed logging to console

--- source/callbacks/teamMember.js
+++ source/callbacks/teamMember.js
@@ -1,6 +1,5 @@
 module.exports = ({ $tag, $page, asset }) => {
     const item = asset.collection.item;
-    console.log("collection!!!", asset.collection);
     $page("title").append(` ${item.firstName} ${item.lastName}`);
     $tag.find("#page-title").append(` #${asset.collection.index + 1}`);
     $tag.find("#name").append(`${item.firstName} ${item.lastName}`);
